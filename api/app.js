37cee9ab7906f42e7783b263a38d6baa3b9925d9
TommyBui89 <126430838+TommyBui89@users.noreply.github.com>
2023-10-11 18:39:57 +1100

    change port 3001 to 3000

--- api/app.js
+++ api/app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3001"
+  origin: "http://localhost:3000"
 };
 
 const db = require("./models");
